947bbfa7f6d486ce00f0085f9728e22f708cbb98
Michael J. Wilson <mwilsonedin@gmail.com>
2013-12-03 18:57:55 +0100

    debugging

--- Plotting/Windowfunc/conv3D.py
+++ Plotting/Windowfunc/conv3D.py
@@ -7,10 +7,10 @@ b      = 0.5
 c      = 0.2
           
 inputPk  = P0*(data[:,0]**2)          
-theory = P0*(data[:,0]**2 + 0.5*(a**-2. + b**-2. + c**-2.))
+theory   = P0*(data[:,0]**2 + 0.5*(a**-2. + b**-2. + c**-2.))
 
-pl.plot(data[:,0], data[:,2], 'r')
-pl.plot(data[:,0], theory,    'b')
-pl.plot(data[:,0], inputPk,   'g')
+pl.loglog(data[:,0], data[:,2], 'r')
+pl.loglog(data[:,0], theory,    'b')
+pl.loglog(data[:,0], inputPk,   'g')
 
 pl.savefig('/disk1/mjw/HOD_MockRun/Plots/ConvolvedPk/3DconvolvedAnisoGauss.pdf')
