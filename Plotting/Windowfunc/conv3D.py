5de08ca205fba39ca669754484fc47cdd02e8f4d
Michael J. Wilson <mwilsonedin@gmail.com>
2013-12-03 18:52:33 +0100

    debugging

--- Plotting/Windowfunc/conv3D.py
+++ Plotting/Windowfunc/conv3D.py
@@ -1,4 +1,4 @@
-data   = np.loadtxt('/disk1/mjw/HOD_MockRun/Data/Pk/midK_Pk_ConvolvedAnsioGauss.dat')
+data   = np.loadtxt('/disk1/mjw/HOD_MockRun/Data/Pk/midK_Pk_ConvolvedAnisoGauss.dat')
 
 a      = 1.0
 b      = 0.5
@@ -6,7 +6,7 @@ c      = 0.2
           
 theory = 150.*(data[:,0]**2 + 0.5*( a**-2. + b**-2. + c**-2. ))
 
-pl.plot(data[:,0], data[:,1], 'r')
+pl.plot(data[:,0], data[:,2], 'r')
 pl.plot(data[:,0], theory,    'b')
 
 pl.savefig('/disk1/mjw/HOD_MockRun/Plots/ConvolvedPk/3DconvolvedAnisoGauss.pdf')
